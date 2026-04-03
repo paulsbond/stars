33484b363faf17629268bd36ee641ffc99bb46f0
Paul Bond <git@paulsbond.co.uk>
2026-04-03 11:18:31 +0100

    Initial stars app

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,108 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useMemo, useRef, useState } from "react";
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+function CalculatePoints(numPoints: number): Point[] {
+  const points: Point[] = [];
+  for (let i = 0; i < numPoints; i++) {
+    const angle = (i / numPoints) * 2 * Math.PI;
+    const x = 250 + 200 * Math.sin(angle);
+    const y = 250 - 200 * Math.cos(angle);
+    points.push({ x, y });
+  }
+  return points;
+}
+
+function DrawCircle(ctx: CanvasRenderingContext2D) {
+  ctx.beginPath();
+  ctx.arc(250, 250, 200, 0, 2 * Math.PI);
+  ctx.strokeStyle = "#333";
+  ctx.stroke();
+}
+
+function DrawStar(
+  ctx: CanvasRenderingContext2D,
+  points: Point[],
+  numSkip: number,
+) {
+  for (let i = 0; i < points.length; i++) {
+    ctx.beginPath();
+    ctx.moveTo(points[i].x, points[i].y);
+    const next = (i + numSkip + 1) % points.length;
+    ctx.lineTo(points[next].x, points[next].y);
+    ctx.strokeStyle = "#eee";
+    ctx.stroke();
+  }
+}
+
+function IntField(props: {
+  label: string;
+  value: number;
+  onChange: (value: number) => void;
+  min?: number;
+  max?: number;
+}) {
+  return (
+    <label className="flex items-center gap-2">
+      <span>{props.label}</span>
+      <input
+        value={props.value}
+        onChange={(e) =>
+          props.onChange(
+            parseInt(e.target.value) === undefined
+              ? props.value
+              : parseInt(e.target.value),
+          )
+        }
+        type="number"
+        min={props.min}
+        max={props.max}
+        className="w-32 rounded-md border border-neutral-700 bg-neutral-800 px-2 py-1"
+      />
+    </label>
+  );
+}
 
 export default function Home() {
+  const [numPoints, setNumPoints] = useState(5);
+  const [numSkip, setNumSkip] = useState(1);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const maxSkips = Math.floor((numPoints - 1) / 2) - 1;
+  const points = useMemo(() => CalculatePoints(numPoints), [numPoints]);
+
+  if (numSkip > maxSkips) setNumSkip(maxSkips);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+    ctx.reset();
+    DrawCircle(ctx);
+    DrawStar(ctx, points, numSkip);
+  }, [canvasRef.current, numPoints, numSkip]);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main className="flex h-svh w-svw flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-end gap-2">
+        <IntField
+          label="Number of Points"
+          value={numPoints}
+          onChange={setNumPoints}
+          min={5}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        <IntField
+          label="Points to Skip"
+          value={numSkip}
+          onChange={setNumSkip}
+          min={1}
+          max={maxSkips}
+        />
+      </div>
+      <canvas width="500" height="500" ref={canvasRef}></canvas>
+    </main>
   );
 }
